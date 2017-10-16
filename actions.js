510358a483bf12ff9fb036f38ff36ddaf481292c
Nicole Rossi <i.am.nicole.rossi@gmail.com>
2017-10-16 07:01:00 -0700

    Hooking up TwitchAPI class to

--- actions.js
+++ actions.js
@@ -1,19 +1,21 @@
-// import TwitchAPI from './scripts/TwitchAPI';
-
 let btnSearch;
 let tiSearchTerm;
 let searchResults;
+let twitchAPI;
 
 function queryTwitchAPI(evt) {
   console.log('actions.js --> queryTwitchAPI');
   console.log(`search term = ${tiSearchTerm.value}`);
-  console.log(TwitchAPI);
+  console.log(twitchAPI);
+  twitchAPI.searchFor(tiSearchTerm.value);
 }
 
 function setupEventListeners (evt) {
   window.removeEventListener('load', setupEventListeners);
   
   console.log('actions.js --> setupEventListeners');
+  
+  twitchAPI = new TwitchAPI();
   
   btnSearch = document.getElementById('search_btn');
   tiSearchTerm = document.getElementById('search_term');
