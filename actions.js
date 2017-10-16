fd3418cbda714db3723b5f4ebd4f90c99eec91f6
Nicole Rossi <i.am.nicole.rossi@gmail.com>
2017-10-16 10:55:56 -0700

    Wrapped in immediate function
    To hide local variables from global scope.  Also need to build a randomized string tail to callback name.

--- actions.js
+++ actions.js
@@ -1,79 +1,88 @@
-function prependZeros(number) {
-  if (number < 10) return `0${number}`;
-  return `${number}`;
-}
-
-function displaySearchResults(results) {
-  console.log('actions.js --> displaySearchResults');
-  console.log(results);
-  
-  const streams = results.streams; 
+(function () {
+  const btnSearch = document.getElementById('search_btn');
+  const searchTerm = document.getElementById('search_term');
   const searchResults = document.getElementById('search_results');
-  
-  const children = searchResults.childNodes;
-  if (children.length > 1) {
-    for (let i = children.length - 1; i > 1; i--) {
-      searchResults.removeChild(children[i]);
+  const topMostHeadTag = document.getElementsByTagName("head")[0];
+  const callbackName = 'displayTwitchAPISearchResults_9410';
+
+  function prependZeros(number) {
+    if (number < 10) return `0${number}`;
+    return `${number}`;
+  }
+
+
+  window[callbackName] = function (results) {
+    console.log('actions.js --> displaySearchResults');
+    console.log(results);
+    
+    const streams = results.streams; 
+    
+    const children = searchResults.childNodes;
+    if (children.length > 1) {
+      for (let i = children.length - 1; i > 1; i--) {
+        searchResults.removeChild(children[i]);
+      }
+    }
+    
+    for(let i = 0; i < streams.length; i++) {
+      const stream = streams[i];
+      const root = document.createElement('article');
+      
+      const thumbnailContainer = document.createElement('div');
+      const summaryContainer = document.createElement('div');
+      root.appendChild(thumbnailContainer);
+      root.appendChild(summaryContainer);
+      
+      const thumbnail = new Image();
+      thumbnail.src = streams[i].preview.small;
+      thumbnailContainer.appendChild(thumbnail);
+      
+      const title = document.createElement('h3');
+      const titleText = document.createTextNode(streams[i].stream_type);
+      title.appendChild(titleText);
+      summaryContainer.appendChild(title);
+      
+      const subTitle = document.createElement('h6');
+      const subTitleText = document.createTextNode(`${streams[i].game} -- ${streams[i].viewers} viewers`);
+      subTitle.appendChild(subTitleText);
+      summaryContainer.appendChild(subTitle);
+      
+      const creationDate = new Date(streams[i].created_at);
+      const year = creationDate.getFullYear();
+      const month = prependZeros(creationDate.getMonth()+1);
+      const day = prependZeros(creationDate.getDate());
+      const hours = prependZeros(creationDate.getHours());
+      const minutes = prependZeros(creationDate.getMinutes());
+      const creationDateStr = `${year}.${month}.${day} at ${hours}:${minutes}`;
+      
+      const description = document.createElement('p');
+      const descriptionText = document.createTextNode(`FPS = ${Math.round(streams[i].average_fps)}; created on ${creationDateStr}`);
+      description.appendChild(descriptionText);
+      summaryContainer.appendChild(description);
+      
+      searchResults.appendChild(root);
     }
   }
-  
-  for(let i = 0; i < streams.length; i++) {
-    const stream = streams[i];
-    const root = document.createElement('article');
+
+  function queryTwitchAPI(offset) {
+    console.log('actions.js --> queryTwitchAPI');
     
-    const thumbnailContainer = document.createElement('div');
-    const summaryContainer = document.createElement('div');
-    root.appendChild(thumbnailContainer);
-    root.appendChild(summaryContainer);
-    
-    const thumbnail = new Image();
-    thumbnail.src = streams[i].preview.small;
-    thumbnailContainer.appendChild(thumbnail);
-    
-    const title = document.createElement('h3');
-    const titleText = document.createTextNode(streams[i].stream_type);
-    title.appendChild(titleText);
-    summaryContainer.appendChild(title);
-    
-    const subTitle = document.createElement('h6');
-    const subTitleText = document.createTextNode(`${streams[i].game} -- ${streams[i].viewers} viewers`);
-    subTitle.appendChild(subTitleText);
-    summaryContainer.appendChild(subTitle);
-    
-    const creationDate = new Date(streams[i].created_at);
-    const year = creationDate.getFullYear();
-    const month = prependZeros(creationDate.getMonth()+1);
-    const day = prependZeros(creationDate.getDate());
-    const hours = prependZeros(creationDate.getHours());
-    const minutes = prependZeros(creationDate.getMinutes());
-    const creationDateStr = `${year}.${month}.${day} at ${hours}:${minutes}`;
-    
-    const description = document.createElement('p');
-    const descriptionText = document.createTextNode(`FPS = ${Math.round(streams[i].average_fps)}; created on ${creationDateStr}`);
-    description.appendChild(descriptionText);
-    summaryContainer.appendChild(description);
-    
-    searchResults.appendChild(root);
+    const newJSONP = document.createElement("script");
+    newJSONP.src = `https://api.twitch.tv/kraken/search/streams?query=${escape(searchTerm.value)}&client_id=bxqhnlfew1j1uqihshtrglvfj2m4f1&callback=${callbackName}&limit=5&offset=${offset}`;
+    topMostHeadTag.appendChild(tag);
   }
-}
 
-function queryTwitchAPI(evt) {
-  console.log('actions.js --> queryTwitchAPI');
-  
-  const tiSearchTerm = document.getElementById('search_term');
-  
-  const tag = document.createElement("script");
-  tag.src = `https://api.twitch.tv/kraken/search/streams?query=${escape(tiSearchTerm.value)}&client_id=bxqhnlfew1j1uqihshtrglvfj2m4f1&callback=displaySearchResults&limit=5`;
-  document.getElementsByTagName("head")[0].appendChild(tag);
-}
+  function newSearchOnTwitchAPI (evt) {
 
-function setupEventListeners (evt) {
-  window.removeEventListener('load', setupEventListeners);
-  
-  console.log('actions.js --> setupEventListeners');
-  
-  const btnSearch = document.getElementById('search_btn');
-  btnSearch.addEventListener('mouseup', queryTwitchAPI);
-}
+  }
 
-window.addEventListener('load', setupEventListeners);
+  function setupEventListeners (evt) {
+    window.removeEventListener('load', setupEventListeners);
+    
+    console.log('actions.js --> setupEventListeners');
+    
+    btnSearch.addEventListener('mouseup', queryTwitchAPI);
+  }
+
+  window.addEventListener('load', setupEventListeners);
+})();
