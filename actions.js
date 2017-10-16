42912c943183b6e13765d155b1db2340ff2e7da1
Nicole Rossi <i.am.nicole.rossi@gmail.com>
2017-10-16 07:57:25 -0700

    Beginning to process JSON

--- actions.js
+++ actions.js
@@ -2,8 +2,11 @@ let btnSearch;
 let tiSearchTerm;
 let searchResults;
 
-function foo() {
-  console.log('actions.js --> foo');
+function displaySearchResults(results) {
+  console.log('actions.js --> displaySearchResults');
+  console.log(results);
+  const resultsObj = JSON.parse(results);
+  console.log(resultsObj);
 }
 
 function queryTwitchAPI(evt) {
@@ -11,7 +14,7 @@ function queryTwitchAPI(evt) {
   console.log(`search term = ${tiSearchTerm.value}`);
   
   const tag = document.createElement("script");
-  tag.src = `https://api.twitch.tv/kraken/search/streams?query=${escape(tiSearchTerm.value)}&client_id=bxqhnlfew1j1uqihshtrglvfj2m4f1&callback=foo`;
+  tag.src = `https://api.twitch.tv/kraken/search/streams?query=${escape(tiSearchTerm.value)}&client_id=bxqhnlfew1j1uqihshtrglvfj2m4f1&callback=displaySearchResults`;
   document.getElementsByTagName("head")[0].appendChild(tag);
 }
 
