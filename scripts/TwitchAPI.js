5c33170cedf977ee8613019dd99050baf782e84e
Nicole Rossi <i.am.nicole.rossi@gmail.com>
2017-10-16 07:08:25 -0700

    Set headers

--- scripts/TwitchAPI.js
+++ scripts/TwitchAPI.js
@@ -2,11 +2,14 @@ class TwitchAPI {
   constructor() {
     console.log('new TwitchAPI');
     this.request = new XMLHttpRequest();
+    this.request.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json');
+    this.request.setRequestHeader('Client-ID', 'bxqhnlfew1j1uqihshtrglvfj2m4f1');
+    this.request.withCredentials = true;
     this.requestListener = this.requestListener.bind(this);
   }
   
   searchFor(searchTerm) {
-    console.log('TwitchAPI --> init: bxqhnlfew1j1uqihshtrglvfj2m4f1');
+    console.log('TwitchAPI --> searchFor');
     
     const {
       request,
@@ -21,6 +24,10 @@ class TwitchAPI {
   requestListener(evt) {
     for(let prop in evt) {
       console.log(`evt[${prop}] = ${evt[prop]}`);
+    }
+    
+    for(let prop in this.request) {
+      console.log(`this.request[${prop}] = ${this.request[prop]}`);
     }
   }
 }
