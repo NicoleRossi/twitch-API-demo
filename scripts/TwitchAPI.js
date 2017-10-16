60798ca2434a0f5979d5d412f838b5fa37bde873
Nicole Rossi <i.am.nicole.rossi@gmail.com>
2017-10-16 06:58:10 -0700

    Added XMLHttpRequest

--- scripts/TwitchAPI.js
+++ scripts/TwitchAPI.js
@@ -1,9 +1,26 @@
 class TwitchAPI {
   constructor() {
     console.log('new TwitchAPI');
+    this.request = new XMLHttpRequest();
+    this.requestListener = this.requestListener.bind(this);
   }
   
-  init() {
-    console.log('TwitchAPI --> init');
+  searchFor(searchTerm) {
+    console.log('TwitchAPI --> init: bxqhnlfew1j1uqihshtrglvfj2m4f1');
+    
+    const {
+      request,
+      requestListener,
+    } = this;
+    
+    request.addEventListener("load", requestListener);
+    request.open("GET", `https://api.twitch.tv/kraken/search/streams?q=${searchTerm}`);
+    request.send();
+  }
+
+  requestListener(evt) {
+    for(let prop in evt) {
+      console.log(`evt[${prop}] = ${evt[prop]}`);
+    }
   }
 }
